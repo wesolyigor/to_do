b07d46ad03be39ea74c363c6c72bf0c22fbb6af8
wesolyigor <wesolowski.igor@gmail.com>
2020-09-09 19:25:43 +0200

    add abc task and task structure

---
+++ task/abc_task.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class ABSTask(ABC):
+
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    @property
+    @abstractmethod
+    def created(self):
+        pass
+
+    @property
+    @abstractmethod
+    def status(self):
+        pass
+
+    @property
+    @abstractmethod
+    def toggle_status(self):
+        pass
+   
---
+++ task/task.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from task.abc_task import ABSTask
+
+
+class Task(ABSTask):
+
+    def __init__(self, name):
+        self._name = name
+        self._status = True
+        self._created = datetime.now()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def created(self):
+        return self._created
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def toggle_status(self):
+        self._status = False if self._status else True
+
+    def __repr__(self):
+        return f'class Task(name: {self.name}, status: {self.status}, created: {self.created})'
+
+    __str__ = __repr__
