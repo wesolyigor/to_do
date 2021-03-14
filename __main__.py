37faa18243904d942779abb525b2d25143bd93f2
wesolyigor <wesolowski.igor@gmail.com>
2021-03-14 18:14:10 +0100

    clean file

--- __main__.py
+++ __main__.py
@@ -20,39 +20,7 @@ commands = (CreateTask, ShowTasks, ToggleStatus, DeleteTask, ShowActiveTask, Sho
 
 app = Cli(commands)  # tworzymy obiekt app klasy CLI
 app.run()  # wywołujemy metodę run obiektu app
-# print(os.environ.get('VERSION')) - sprawdzamy czy działa wczytywanie zmiennych środowiskowych
 
-# cammelcase - piszemy clasy, dlatego w tej tupli
-
-
-# get_user_command()
-
-# def get_commands():
-#     _commands_old = (AddTask,)
-#     return AddTask
-#
-#
-# command = get_commands()
-# command.action()
-
-# for i in range(10):
-#     dashboard.add_task(Task(f'kebab {i}', lc_uuid))
-#
-# for t in dashboard:
-#     print(t)
-#
-# print('*' * 20)
-
-# dashboard.delete_task(7)
-# dashboard.delete_task(2)
-# print(dashboard.get_task(8))
-# dashboard.add_task(Task('cokolwiek', lc_uuid))
-# # dashboard.update_task(9, Task('costam', lc_uuid))
-# # dashboard.get_task(9).toggle_status()
-# print(dashboard.get_task(9).due_time)
 
 print('*' * 20)
-
-# for t in dashboard:
-#     print(t)
 
